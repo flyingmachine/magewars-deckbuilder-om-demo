41ef36212441682f7054c049b1ac8b2abfc479c7
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2015-06-08 14:49:35 -0400

    little update script

---
+++ update.sh
@@ -0,0 +1,4 @@
+cp ../magewars-spellbook/target/index.html ./
+cp ../magewars-spellbook/target/main.js ./
+cp -R ../magewars-spellbook/target/images ./
+cp -R ../magewars-spellbook/target/stylesheets ./
